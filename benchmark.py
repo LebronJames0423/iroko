93bbbe64f0d0b61e9377867919eda55aead67c25
张翼 <zhangyi19941217@gmail.com>
2019-04-04 14:02:01 +0800

    Update benchmark.py

--- benchmark.py
+++ benchmark.py
@@ -1,3 +1,4 @@
+                                                                                                                                                                                    97,1          Bot
 from __future__ import print_function
 import os
 import subprocess
@@ -46,5 +47,51 @@ def generate_testname(output_dir):
     testname = "%s_%s" % (socket.gethostname(), n_folders)
     return testname
 
-
 def dump_config(path):
+    test_config = {}
+    test_config["transport"] = TRANSPORT
+    test_config["timesteps"] = STEPS
+    test_config["runs"] = RUNS
+    test_config["topology"] = TOPO
+    test_config["algorithms"] = ALGOS
+    # Get a string formatted time stamp
+    ts = time.time()
+    st = datetime.datetime.fromtimestamp(ts).strftime('%Y_%m_%d_%H_%M_%S')
+    test_config["timestamp"] = st
+    with open(path + "/test_config.json", 'w') as fp:
+        json.dump(test_config, fp)
+
+
+def run_tests():
+    testname = generate_testname(OUTPUT_DIR)
+    results_dir = "%s/%s" % (OUTPUT_DIR, testname)
+    print("Saving results to %s" % results_dir)
+    check_dir(results_dir)
+    print("Dumping configuration in %s" % results_dir)
+    dump_config(results_dir)
+    for transport in TRANSPORT:
+        for index in range(RUNS):
+            results_subdir = "%s/%s/run%d" % (results_dir, transport, index)
+            for algo in ALGOS:
+                cmd = "sudo python run_ray.py "
+                cmd += "-a %s " % algo
+                cmd += "-t %d " % STEPS
+                cmd += "--output %s " % results_subdir
+                cmd += "--topo %s " % TOPO
+                if TUNE:
+                    cmd += "--tune "
+                if RESTORE:
+                    cmd += "--restore %s " % RESTORE_PATH
+                # always use TCP if we are dealing with a TCP algorithm
+                if algo in TCP_ALGOS:
+                    cmd += "--transport tcp "
+                else:
+                    cmd += "--transport %s " % transport
+                subprocess.call(cmd.split())
+    # Plot the results and save the graphs under the given test name
+    plot(results_dir, PLOT_DIR, testname)
+
+
+if __name__ == '__main__':
+    # Start pre-defined tests
+    run_tests()
